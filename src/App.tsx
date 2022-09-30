c19c615835417840cea0fd76c2a0f2810de4029a
Hoang Duong <hoangduongphantri@gmail.com>
2022-09-30 17:42:00 +0300

    First update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Home from "./Home";
 import React from "react";
 
 const App = (): JSX.Element => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 };
 
 export default App;
