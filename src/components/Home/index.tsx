950a1f5e7d442be92980d0910125fadbb270e9ed
Hoang Duong <hoangduongphantri@gmail.com>
2022-10-06 16:15:45 +0300

    Implement 'Chatbot' component

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,14 +1,24 @@
 import "./styles.scss";
 
+import React, { useState } from "react";
+
 import Button from "../Buttons";
-import React from "react";
+import Chatbot from "../Chatbot";
 
 const Home = () => {
+  const [chatOn, setChatOn] = useState<boolean>(false);
+  const toggleButton = (): void => {
+    setChatOn(!chatOn);
+  };
   return (
     <div className="home-page">
       Look at the bottom-right corner{" "}
       <div className="chatbot-area">
-        <Button />
+        {chatOn && <Chatbot />}
+
+        <div className="button-area">
+          <Button onClick={() => toggleButton()} chatOn={chatOn} />
+        </div>
       </div>
     </div>
   );
