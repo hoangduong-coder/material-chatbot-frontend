9fd4dbe7c6e77c3647292362e6b7e892e729c3f2
Hoang Duong <hoangduongphantri@gmail.com>
2022-10-06 16:15:27 +0300

    Re-arrange the code structure

--- src/components/Buttons/index.tsx
+++ src/components/Buttons/index.tsx
@@ -1,19 +1,19 @@
 import "./styles.scss";
 
-import React, { useState } from "react";
-
 import ChatRoundedIcon from "@mui/icons-material/ChatRounded";
 import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import React from "react";
 
-const Button = () => {
-  const [isClicked, setIsClicked] = useState<boolean>(false);
-  const toggleButton = (): void => {
-    setIsClicked(!isClicked);
-  };
+interface ButtonProps {
+  chatOn: boolean;
+  onClick: any;
+}
+
+const Button = ({ chatOn, onClick }: ButtonProps) => {
   return (
     <div className="main-button">
-      <button onClick={toggleButton} className="icon">
-        {!isClicked ? (
+      <button onClick={onClick} className="icon">
+        {!chatOn ? (
           <ChatRoundedIcon style={{ color: "#ffffff" }} />
         ) : (
           <CloseRoundedIcon style={{ color: "#ffffff" }} />
