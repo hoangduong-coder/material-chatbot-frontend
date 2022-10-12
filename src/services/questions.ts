fcb4786b43d986766dce4b207ac99bea7cde110a
Hoang Duong <hoangduongphantri@gmail.com>
2022-10-12 14:45:17 +0300

    Update the URL and the type of data in the post method

--- src/services/questions.ts
+++ src/services/questions.ts
@@ -2,19 +2,18 @@ import { Answer, Question } from "../types";
 
 import axios from "axios";
 
-const baseUrl = `https://${process.env["LANGUAGE_STUDIO_ENDPOINT"]}.api.cognitive.microsoft.com//language/:query-knowledgebases?projectName=material-chatbot&api-version=2021-10-01&deploymentName=test`;
+const baseUrl = `https://${process.env.REACT_APP_ENDPOINT}.cognitiveservices.azure.com//language/:query-knowledgebases?projectName=material-chatbot&api-version=2021-10-01&deploymentName=test`;
 
 const postQuestion = async (question: Question) => {
-  const { data } = await axios.post<Answer[]>(baseUrl, question, {
+  const { data } = await axios.post<{ answers: Answer[] }>(baseUrl, question, {
     headers: {
       "Content-Type": "application/json",
-      "Ocp-Apim-Subscription-Key": process.env["LANGUAGE_STUDIO_API_KEY"],
+      "Ocp-Apim-Subscription-Key": process.env.REACT_APP_API_KEY,
     },
   });
-  console.log(JSON.stringify(data, null, 4));
   return data;
 };
 
 export default {
-  postQuestion,
+  postQuestion: postQuestion,
 };
