ffe1b29de90628941b269a65d272046126c50cc9
Hoang Duong <hoangduongphantri@gmail.com>
2022-10-18 00:31:27 +0300

    Design header basic structure

--- src/components/Chatbot/Chatbot-header.tsx
+++ src/components/Chatbot/Chatbot-header.tsx
@@ -1,13 +1,22 @@
+import { Avatar } from "@mui/material";
 import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import HorizontalRuleIcon from "@mui/icons-material/HorizontalRule";
 import React from "react";
+import logo from "../../assets/small-logo.gif";
 
 const ChatbotHeader = () => {
   return (
-    <div>
-      Material-bot{" "}
-      <span>
+    <div className="chatbot-header">
+      <div className="header-left">
+        <div className="logo">
+          <Avatar src={logo} alt="logo" sx={{ width: 40, height: 40 }} />
+        </div>
+        <p>Wärtsilä cooperations </p>
+      </div>
+      <div className="header-right">
+        <HorizontalRuleIcon />
         <CloseRoundedIcon />
-      </span>
+      </div>
     </div>
   );
 };
