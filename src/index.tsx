fd88c4449c3c55970884f1b59a265c27b86955ed
Hoang Duong <hoangduongphantri@gmail.com>
2022-10-20 00:22:45 +0300

    Wrap Provider component

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import App from "./App";
+import { Provider } from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { store } from "./services/store/store";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
