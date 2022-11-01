efddc7b8cb20a3e48531ddf6556d7af25e092157
Hoang Duong <hoangduongphantri@gmail.com>
2022-11-02 00:03:57 +0200

    Modify chatbot body

--- src/components/Chatbot/Chatbot-body.tsx
+++ src/components/Chatbot/Chatbot-body.tsx
@@ -1,11 +1,22 @@
+import { Avatar } from "@mui/material";
 import ChatBubble from "../Chat-bubbles";
 import React from "react";
+import logo from "../../assets/small-logo.gif";
 import { useAppSelector } from "../../services/slices/hooks";
 
 const ChatbotBody = () => {
   const chatLog = useAppSelector((state) => state.chatLog.allChat);
   return (
     <div className="chatbot-body">
+      <div className="title">
+        <Avatar
+          src={logo}
+          alt="Wartsila's logo"
+          sx={{ width: 80, height: 80 }}
+        />
+        <h2>Wärtsilä cooperations</h2>
+      </div>
+
       {chatLog.map((obj) =>
         obj.title === "QUESTION" ? (
           <ChatBubble
