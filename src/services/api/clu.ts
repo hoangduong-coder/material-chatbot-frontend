03c7b0ea818f2ff101786a3cab2bab3b0bd8909e
Hoang Duong <hoangduongphantri@gmail.com>
2022-12-04 23:11:56 +0200

    disable import/no-anonymous-default-export

--- src/services/api/clu.ts
+++ src/services/api/clu.ts
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-anonymous-default-export */
+
 import QueryModels from "../../types/searchQuery";
 import { Question } from "../../types";
 import axios from "axios";
--- src/services/api/qna-search.ts
+++ src/services/api/qna-search.ts
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-anonymous-default-export */
+
 import { Answer, QueryModels } from "../../types";
 
 import axios from "axios";
