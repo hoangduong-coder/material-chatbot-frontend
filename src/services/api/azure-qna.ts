140c7080663f506ffc88c51e1b36a617501975d9
Hoang Duong <hoangduongphantri@gmail.com>
2022-12-08 16:24:45 +0200

    Change Azure QnA url

--- src/services/api/azure-qna.ts
+++ src/services/api/azure-qna.ts
@@ -4,18 +4,21 @@ import { Answer, Question } from "../../types";
 
 import axios from "axios";
 
-const baseUrl = `https://${process.env.REACT_APP_ENDPOINT}.cognitiveservices.azure.com/language/:query-knowledgebases?projectName=material-chatbot-qna-2&api-version=2021-10-01&deploymentName=test`;
-
 const postQuestion = async (question: Question) => {
-  const { data } = await axios.post<{ answers: Answer[] }>(baseUrl, question, {
-    headers: {
-      "Content-Type": "application/json",
-      "Ocp-Apim-Subscription-Key": process.env.REACT_APP_API_KEY,
-    },
-  });
+  const { data } = await axios.post<{ answers: Answer[] }>(
+    //@ts-ignore
+    process.env.REACT_APP_AZURE_QNA_URL,
+    question,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "Ocp-Apim-Subscription-Key": process.env.REACT_APP_API_KEY,
+      },
+    }
+  );
   return data;
 };
 
 export default {
-  postQuestion: postQuestion,
+  postStaticQuestion: postQuestion,
 };
